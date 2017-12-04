2ec0830880b9dcb6e4df13346146e5ae5626630b
mohit-nodexpert <mohit.d@successivesoftwares.com>
2017-12-04 19:52:41 +0530

    added reactdict for temporary hiding of done tasks

--- todo-list/imports/ui/body.js
+++ todo-list/imports/ui/body.js
@@ -2,13 +2,31 @@ import { Template } from 'meteor/templating';
  
 import { Tasks } from '../api/tasks.js';
 
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import './task.js';
 
 import './body.html';
- 
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     return Tasks.find({});
+  },
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  },
+  completeCount() {
+    return Tasks.find({}).count();
   },
 });
 Template.body.events({
@@ -28,6 +46,9 @@ Template.body.events({
  
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
 
